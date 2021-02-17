8737a6504b297c02e7dce0856f2bb83bf57bd3c4
Shadow0144 <CorbinCogswell@gmail.com>
2021-02-17 01:40:11 -0600

    Code for changing the texture of undug lead

--- LeadColorFixMod/Patches.cs
+++ LeadColorFixMod/Patches.cs
@@ -9,6 +9,11 @@ using UnityEngine;
 
 namespace LeadColorFixMod
 {
+    /*public static String ModPath()
+    {
+        return Directory.GetParent(Assembly.GetExecutingAssembly().Location) + Path.DirectorySeparatorChar.ToString();
+    }*/
+
     /*[HarmonyPatch(typeof(Assets))]
     [HarmonyPatch(nameof(Assets.GetSprite))]
    
@@ -42,13 +47,12 @@ namespace LeadColorFixMod
         }
     }*/
 
-    [HarmonyPatch(typeof(SubstanceTable))]
+    /*[HarmonyPatch(typeof(SubstanceTable))]
     [HarmonyPatch(nameof(SubstanceTable.GetSubstance))]
     internal static class Patch_SubstanceTable_GetSubstance
     {
         private static Substance Postfix(Substance __result, SimHashes substance)
         {
-            Debug.Log("Patching:" + substance.ToString());
             if (substance == SimHashes.Lead)
             {
                 Debug.Log("Entering lead patch!");
@@ -58,7 +62,7 @@ namespace LeadColorFixMod
                 Texture2D leadTexture = null;
                 foreach (Texture2D texture in allTextures)
                 {
-                    if (texture.name.Equals("lead_0"))
+                    if (texture.name.Equals("aluminum_0"))
                     {
                         leadTexture = texture;
                         break;
@@ -72,11 +76,11 @@ namespace LeadColorFixMod
                 TextAsset leadBuild = null;
                 foreach (TextAsset text in allTextAssets)
                 {
-                    if (text.name == "lead_anim")
+                    if (text.name == "aluminum_anim")
                     {
                         leadAnim = text;
                     }
-                    else if (text.name == "lead_build")
+                    else if (text.name == "aluminum_build")
                     {
                         leadBuild = text;
                     }
@@ -106,21 +110,16 @@ namespace LeadColorFixMod
 
             return __result;
         }
-    }
+    }*/
 
 
     [HarmonyPatch(typeof(ElementLoader))]
     [HarmonyPatch(nameof(ElementLoader.Load))]
     internal static class Patch_ElementLoader_Load
     {
-        public static String ModPath()
-        {
-            return Directory.GetParent(Assembly.GetExecutingAssembly().Location) + Path.DirectorySeparatorChar.ToString();
-        }
-
         private static void Postfix(ref Hashtable substanceList, SubstanceTable substanceTable)
         {
-            /*Debug.Log("Updating KAnims...");
+            Debug.Log("Updating KAnims...");
 
             //Debug.Log("Lead: " + (substanceTable.GetSubstance(SimHashes.Lead) != null));
             //Debug.Log("Lead material: " + substanceTable.GetSubstance(SimHashes.Lead).material.name);
@@ -130,10 +129,8 @@ namespace LeadColorFixMod
             Substance gold = substanceTable.GetSubstance(SimHashes.Gold);
             Substance copper = substanceTable.GetSubstance(SimHashes.Copper);
 
-            Texture2D leadTexture = Resources.Load<Texture2D>("Texture2D/lead_0");
+            Texture2D leadTexture = null;
             Texture2D[] allTextures = Resources.FindObjectsOfTypeAll<Texture2D>();
-            Debug.Log("T: " + (Resources.GetBuiltinResource<Texture2D>("Texture2D/lead_0") == null));
-            Debug.Log("nT: " + (Resources.GetBuiltinResource<Texture2D>("lead_0") == null));
             TextAsset[] allTextAssets = Resources.FindObjectsOfTypeAll<TextAsset>();
             
             foreach (Texture2D texture in allTextures)
@@ -145,6 +142,8 @@ namespace LeadColorFixMod
                 }
                 else { }
             }
+            List<Texture2D> leadTextures = new List<Texture2D>();
+            leadTextures.Add(leadTexture);
 
             bool leadAnimSet = false;
             bool leadBuildSet = false;
@@ -152,17 +151,20 @@ namespace LeadColorFixMod
             TextAsset leadBuild = null;
             foreach (TextAsset text in allTextAssets)
             {
-                if (text.name == "lead_anim")
+                if (text.name.Equals("lead_anim"))
                 {
                     leadAnim = text;
+                    leadAnimSet = true;
                 }
-                else if (text.name == "lead_build")
+                else if (text.name.Equals("lead_build"))
                 {
                     leadBuild = text;
+                    leadBuildSet = true;
                 }
                 else { }
                 if (leadAnimSet && leadBuildSet)
                 {
+                    Debug.Log("TextAssets found");
                     break;
                 }
                 else { }
@@ -170,11 +172,37 @@ namespace LeadColorFixMod
 
             if (leadTexture != null && leadAnim != null && leadBuild != null)
             {
-                lead.anim.mod = new KAnimFile.Mod();
+                /*lead.anim.mod = new KAnimFile.Mod();
                 lead.anim.mod.textures = new List<Texture2D>();
                 lead.anim.mod.textures.Add(leadTexture);
                 lead.anim.mod.anim = leadAnim.bytes;
-                lead.anim.mod.build = leadBuild.bytes;
+                lead.anim.mod.build = leadBuild.bytes;*/
+
+                lead.anim.Initialize(leadAnim, leadBuild, leadTextures);
+
+                KAnimFile[] anims = new KAnimFile[1];
+                lead.GetType().GetField("anims", System.Reflection.BindingFlags.NonPublic
+                                                         | System.Reflection.BindingFlags.Instance)
+                     .SetValue(lead, anims);
+
+                /*Debug.Log("Trying...");
+                
+                lead.anim.GetType().GetField("textures", System.Reflection.BindingFlags.NonPublic
+                                                         | System.Reflection.BindingFlags.Instance)
+                     .SetValue(lead.anim, textures);
+
+                Debug.Log("Trying...");
+
+                lead.anim.GetType().GetField("animBytes", System.Reflection.BindingFlags.NonPublic 
+                                                         | System.Reflection.BindingFlags.Instance)
+                    .SetValue(lead.anim, leadAnim.bytes);
+
+                Debug.Log("Trying...");
+
+                lead.anim.GetType().GetField("buildBytes", System.Reflection.BindingFlags.NonPublic
+                                                         | System.Reflection.BindingFlags.Instance)
+                    .SetValue(lead.anim, leadBuild.bytes);*/
+
                 Debug.Log("Texture Success");
             }
             else
@@ -183,17 +211,28 @@ namespace LeadColorFixMod
             }
 
             substanceList[lead.elementID] = lead;
-            substanceTable.OnAfterDeserialize();*/
 
-            /*Color32 leadColor = new Color32(156, 166, 181, 255);
-            Color32 leadShineColor = new Color32(208, 217, 222, 255);
-            Color32 leadSpecColor = new Color32(165, 171, 192, 255);
-            Color32 leadUIColor = new Color32(102, 2, 60, 255);
+            Element leadElement = ElementLoader.FindElementByHash(lead.elementID);
+            leadElement.substance.anim.Initialize(leadAnim, leadBuild, leadTextures);
 
-            lead.colour = leadColor;
-            lead.uiColour = leadUIColor;
+            // The material affects the undug texture
+            lead.material = new Material(leadElement.substance.material.shader);
+            lead.material.CopyPropertiesFromMaterial(gold.material);
+            lead.material.mainTexture = leadTexture;
 
-            Material leadMaterial = new Material(lead.material);
+            //BindAnimList()
+            var list = substanceTable.GetList();
+            foreach (Substance s in list)
+            {
+                if (s.name.Equals("Lead"))
+                {
+                    s.anim.Initialize(leadAnim, leadBuild, leadTextures);
+                    Debug.Log("Anim: " + s.anim.name);
+                }
+            }
+            //substanceTable.GetType().GetMethod("BindAnimList", BindingFlags.NonPublic).Invoke(substanceTable, null);
+
+            /*Material leadMaterial = new Material(lead.material);
             Debug.Log("Original material name: " + lead.material.name);
             Debug.Log("Original material texture: " + lead.material.mainTexture.name);
             leadMaterial.name = "matLead";
@@ -202,40 +241,91 @@ namespace LeadColorFixMod
             leadMaterial.SetColor("_SpecColor", leadSpecColor);
             lead.material = leadMaterial;
 
-            Texture2D[] textures = Resources.FindObjectsOfTypeAll<Texture2D>();
-            for (int i = 0; i < textures.Length; i++)
-            {
-                //Debug.Log(textures[i].name);
-                if (textures[i].name.Equals("lead_0"))
-                {
-                    //leadMaterial.SetTexture("_MainTex", textures[i]);
-                    substanceTable.GetSubstance(SimHashes.Lead).anim.textureList[0] = textures[i];
-                    lead.anim.textureList[0] = textures[i];
-                    Debug.Log("Success");
-                    break;
-                }
-            }
-            //lead.RefreshPropertyBlock();
-
             Element leadElement = ElementLoader.FindElementByHash(SimHashes.Lead);
             Debug.Log("Did it work? " + (leadElement != null));
-            leadElement.substance.material = leadMaterial;
+            leadElement.substance.material = leadMaterial;*/
+
+            Color32 leadColor = new Color32(156, 166, 181, 255);
+            Color32 leadShineColor = new Color32(208, 217, 222, 255);
+            Color32 leadSpecColor = new Color32(165, 171, 192, 255);
+            Color32 leadUIColor = new Color32(102, 2, 60, 255);
 
             Substance moltenLead = substanceTable.GetSubstance(SimHashes.MoltenLead);
-
-            moltenLead.uiColour.r = 102;
-            moltenLead.uiColour.g = 2;
-            moltenLead.uiColour.b = 60;
-
-            moltenLead.material = leadMaterial;
-
             Substance leadGas = substanceTable.GetSubstance(SimHashes.LeadGas);
 
-            leadGas.uiColour.r = 102;
-            leadGas.uiColour.g = 2;
-            leadGas.uiColour.b = 60;
+            lead.uiColour = leadUIColor;
+            moltenLead.uiColour = leadUIColor;
+            //moltenLead.material.color = leadColor;
+            leadGas.uiColour = leadUIColor;
+            //leadGas.material.color = leadColor;
+        }
+    }
 
-            leadGas.material = leadMaterial;*/
+    [HarmonyPatch(typeof(Db))]
+    [HarmonyPatch(nameof(Db.Initialize))]
+    public class Patch_Db_Initialize
+    {
+        public static void Postfix()
+        {
+            Debug.Log("Db.Initialize");
+            Dictionary<HashedString, KAnimFile> table = Traverse.Create<Assets>().Field("AnimTable").GetValue<Dictionary<HashedString, KAnimFile>>();
+
+            Debug.Log("Contains: " + table.ContainsKey("lead_kanim"));
+
+            Debug.Log("Texture: " + table["lead_kanim"].textureList[0].name);
+
+            table["lead_kanim"] = Assets.GetAnim("copper_refined_kanim");
+            table["gold_refined_kanim"] = Assets.GetAnim("aluminum_refined_kanim");
+
+            ////
+            Texture2D leadTexture = null;
+            Texture2D[] allTextures = Resources.FindObjectsOfTypeAll<Texture2D>();
+            TextAsset[] allTextAssets = Resources.FindObjectsOfTypeAll<TextAsset>();
+
+            foreach (Texture2D texture in allTextures)
+            {
+                if (texture.name.Equals("lead_0"))
+                {
+                    leadTexture = texture;
+                    break;
+                }
+                else { }
+            }
+            List<Texture2D> leadTextures = new List<Texture2D>();
+            leadTextures.Add(leadTexture);
+
+            bool leadAnimSet = false;
+            bool leadBuildSet = false;
+            TextAsset leadAnim = null;
+            TextAsset leadBuild = null;
+            foreach (TextAsset text in allTextAssets)
+            {
+                if (text.name.Equals("lead_anim"))
+                {
+                    leadAnim = text;
+                    leadAnimSet = true;
+                }
+                else if (text.name.Equals("lead_build"))
+                {
+                    leadBuild = text;
+                    leadBuildSet = true;
+                }
+                else { }
+                if (leadAnimSet && leadBuildSet)
+                {
+                    Debug.Log("TextAssets found");
+                    break;
+                }
+                else { }
+            }
+
+            Element leadElement = ElementLoader.FindElementByHash(SimHashes.Lead);
+            leadElement.substance.anim.Initialize(leadAnim, leadBuild, leadTextures);
+            /////
+
+            //var foo = typeof(SubstanceTable).GetMethod("MethodName", BindingFlags.NonPublic | BindingFlags.Instance(or Static));
+            //foo.Invoke(null, null);
+
         }
     }
 }
